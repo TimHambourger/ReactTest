660d8df21dff868e41c76523be3eab647df5364d
tim_hambourger <thambourger@osintegrators.com>
2015-04-16 23:17:40 -0400

    Add dev vs prod gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    reactify = require('reactify');
+    reactify = require('reactify'),
+    uglify = require('gulp-uglify'),
+    vinylBuffer = require('vinyl-buffer');
+
+var PRODUCTION = true;
 
 gulp.task('browserify', function () {
-    return browserify({
+    var bundle = browserify({
             entries: './src/appStart.jsx',
-            debug: true,
+            debug: !PRODUCTION,
             extensions: '.jsx'
         })
         .transform(reactify, { es6: true })
         .bundle()
-        .pipe(source('appStart.js'))
-        .pipe(gulp.dest('./build'));
+        .pipe(source('appStart.js'));
+
+    if (PRODUCTION) bundle = bundle
+        .pipe(vinylBuffer())
+        .pipe(uglify());
+
+    return bundle.pipe(gulp.dest('./build'));
 });
+
+gulp.task('set-dev', function () {
+    PRODUCTION = false;
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/*', './lib/*'], ['set-dev', 'default']);
+});
+
+gulp.task('default', ['browserify']);
+
+gulp.task('dev', ['watch', 'set-dev', 'default']);
